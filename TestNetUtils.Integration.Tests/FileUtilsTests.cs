e6c3dcb06983a31763000e01a24c68ba686a4582
eduardosantosgit <eduardosantos058@gmail.com>
2018-05-06 20:28:26 -0300

    add init test method opentext

---
+++ TestNetUtils.Integration.Tests/FileUtilsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestNetUtils.Integration.Tests
+{
+    [Trait("Integration Tests", "FileUtilsTests")]
+    public class FileUtilsTests
+    {
+
+        [Fact(DisplayName = "OpenText Name File Return String file")]
+        public void OpenText_WhenNameFile_ReturnsStringFile()
+        {
+            //arrange
+            var name = "NameFile.txt";
+
+            var file = FileUtils.OpenText(name);
+
+            Assert.NotNull(file);
+        }
+
+    }
+}
