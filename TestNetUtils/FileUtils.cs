e8066013941e9790ca938a7f35c51b8adfddb702
eduardosantosgit <eduardosantos058@gmail.com>
2018-05-06 20:28:11 -0300

    alter method opentext

--- TestNetUtils/FileUtils.cs
+++ TestNetUtils/FileUtils.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace TestNetUtils
@@ -9,15 +9,45 @@ namespace TestNetUtils
     public static class FileUtils
     {
 
-        public static StreamReader OpenText(String path)
+        public static StreamReader OpenText(string source)
         {
-            if (path == null)
-                throw new ArgumentNullException("path");
-            Contract.EndContractBlock();
-            return new StreamReader(path);
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (IsPath(source))
+                return new StreamReader(source);
+
+            var file = FindFileAllDirectory(source);
+
+            return null;
+            
         }
 
+        private static bool IsPath(string directory)
+        {
+            return (directory.Contains("\\") || directory.Contains(":"));
+        }
 
+        private static string FindFileAllDirectory(string name)
+        {
+
+            var location = typeof(FileUtils).GetTypeInfo().Assembly.Location;
+            var dirPath = Path.GetDirectoryName(location);
+
+            if (File.Exists(Path.Combine(dirPath, name)) == false)
+            {
+                if (File.Exists(Path.Combine(dirPath, $"..\\{name}")))
+                {
+                    name = $"..\\{name}";
+                }
+                else if (File.Exists(Path.Combine(dirPath, $"{(name.StartsWith("..\\") ? name.Remove(0, 3) : name)}")))
+                {
+                    name = name.Remove(0, 3);
+                }
+            }
+
+            return null;
+        }
 
     }
 }
