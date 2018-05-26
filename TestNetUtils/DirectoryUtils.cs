cf4f55c356baf12c8046ceec5ecb185f5678c5bb
EduardoSantosGit <eduardosantos058@gmail.com>
2018-05-26 11:00:00 +0100

    init class directory utils

---
+++ TestNetUtils/DirectoryUtils.cs
@@ -0,0 +1,17 @@
+namespace TestNetUtils
+{
+
+    public static class DirectoryUtils
+    {
+
+        public string CreateDirectory(string name){
+            return null;
+        }
+
+        public string RemoveDirectory(string name){
+            return null;
+        }
+
+    }
+
+}
