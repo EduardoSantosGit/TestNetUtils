fc383adafb7749edb4e27a9915e4f863e48404e1
eduardosantosgit <eduardosantos058@gmail.com>
2018-06-12 20:01:51 -0300

    add class test directory

---
+++ TestNetUtils.Integration.Tests/DirectoryUtilsTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestNetUtils.Integration.Tests
+{
+    [Trait("Integration Tests", "DirectoryUtilsTests")]
+    public class DirectoryUtilsTests
+    {
+
+        [Fact(DisplayName = "Send Name directory Returns Path Directory")]
+        public void OpenText_WhenNameFile_ReturnsStringFile()
+        {
+           
+        }
+
+
+    }
+}
